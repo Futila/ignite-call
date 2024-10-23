16affe848a0f917121707f90f25ae48bbd899a53
Fernando Futila <fernando.futila@gmail.com>
2024-10-23 00:06:20 -0300

    Conexão com calendário - Permissão de acesso ao calendário

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,8 +6,31 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      // choosing the scope/permissions
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+
+  // in case the user does not permit
+  callbacks: {
+    // this functions is called in the moment the user logged in the application
+    async signIn({ account }) {
+      // scope are the permissions the user gave to our application
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      //
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
